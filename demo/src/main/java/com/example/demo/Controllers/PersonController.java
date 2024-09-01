5422b2a282d71a3275c39f3c9cc435b6ccb64e2b
Mohammed <mohammed@Zipcoders-MacBook-Pro-9.local>
2024-09-01 07:16:29 -0400

    created readyByid method in person controller

--- demo/src/main/java/com/example/demo/Controllers/PersonController.java
+++ demo/src/main/java/com/example/demo/Controllers/PersonController.java
@@ -5,10 +5,7 @@ import com.example.demo.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value="/person-controller")
@@ -19,10 +16,17 @@ public class PersonController {
     public PersonController (@Autowired PersonService personService){
         this.personService = personService;
     }
+    // create, readbyid, readall, update, delete
     @RequestMapping(path="/create", method= RequestMethod.POST)
     public ResponseEntity<Person>create(@RequestBody Person person){
     Person requestBody = personService.create(person);
     ResponseEntity<Person>responseEntity = new ResponseEntity<>(requestBody, HttpStatus.CREATED);
     return responseEntity;
+    }
+    @RequestMapping(path="/read/{id}", method=RequestMethod.GET)
+    public ResponseEntity<Person>readById(@PathVariable Long id){
+        Person requestBody= personService.readById(id);
+        ResponseEntity<Person>responseEntity = new ResponseEntity<>(requestBody, HttpStatus.OK);
+        return responseEntity;
     }
 }
