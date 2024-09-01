827eb30a2c338a1a482b2d531fd3cdd87a1739fa
Mohammed <mohammed@Zipcoders-MacBook-Pro-9.local>
2024-09-01 07:36:41 -0400

    added delete, update, findbyid, and findall methods to person controller class

--- demo/src/main/java/com/example/demo/Controllers/PersonController.java
+++ demo/src/main/java/com/example/demo/Controllers/PersonController.java
@@ -3,9 +3,12 @@ package com.example.demo.Controllers;
 import com.example.demo.Services.PersonService;
 import com.example.demo.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/person-controller")
@@ -19,14 +22,35 @@ public class PersonController {
     // create, readbyid, readall, update, delete
     @RequestMapping(path="/create", method= RequestMethod.POST)
     public ResponseEntity<Person>create(@RequestBody Person person){
-    Person requestBody = personService.create(person);
-    ResponseEntity<Person>responseEntity = new ResponseEntity<>(requestBody, HttpStatus.CREATED);
+    final Person requestBody = personService.create(person);
+    final ResponseEntity<Person>responseEntity = new ResponseEntity<>(requestBody, HttpStatus.CREATED);
     return responseEntity;
     }
     @RequestMapping(path="/read/{id}", method=RequestMethod.GET)
     public ResponseEntity<Person>readById(@PathVariable Long id){
-        Person requestBody= personService.readById(id);
-        ResponseEntity<Person>responseEntity = new ResponseEntity<>(requestBody, HttpStatus.OK);
+       final Person requestBody= personService.readById(id);
+        final ResponseEntity<Person>responseEntity = new ResponseEntity<>(requestBody, HttpStatus.OK);
+        return responseEntity;
+    }
+
+    @RequestMapping(path="/readall", method=RequestMethod.GET)
+    public ResponseEntity<List<Person>> readAll(){
+       final List<Person>peopleList = personService.readAll();
+       final ResponseEntity<List<Person>> responseEntity = new ResponseEntity<>(peopleList, HttpStatus.OK);
+       return responseEntity;
+    }
+
+    @RequestMapping(path="/update/{id}", method=RequestMethod.PUT)
+    public ResponseEntity<Person> update(@RequestBody Person person, @PathVariable Long id){
+        final Person p = personService.update(id, person);
+        final ResponseEntity<Person> responseEntity = new ResponseEntity<>(p, HttpStatus.OK);
+        return responseEntity;
+    }
+
+    @RequestMapping(path="/delete/{id}", method=RequestMethod.DELETE)
+    public ResponseEntity<Person> delete(@PathVariable Long id){
+        final Person responseBody = personService.deleteById(id);
+        final ResponseEntity<Person> responseEntity = new ResponseEntity<>(responseBody, HttpStatus.OK);
         return responseEntity;
     }
 }
